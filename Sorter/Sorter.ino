d5e31f591a071ec11f1a1ca6494505a5d9638176
jjv432 <jackvranicar43@gmail.com>
2024-12-03 17:21:56 -0500

    Working sorter

--- Sorter/Sorter.ino
+++ Sorter/Sorter.ino
@@ -1,5 +1,5 @@
-int dt = 6;
-float breakBeam = 0;
+int stepper_dt = 6;
+float breakBeamVal = 0;
 float tallBreakBeam = 0;
 int state = 1;
 int t = 0;
@@ -8,6 +8,12 @@ int duty, prevState;
 const int beamTol = 500;
 int curPos = 0;
 int desPos = 0;
+int t_not;
+int button_delay = 100;
+
+int switch1Value, switch1OldValue;
+bool button = 1;
+bool breakBeam = 0;
 
 void setup() {
 
@@ -16,54 +22,64 @@ void setup() {
   DDRA = 255;
   PORTA = 0b1;
   Serial.begin(9600);
+
+  DDRC = 0;
+  PORTC = 0xFF;
+  switch1OldValue = PINC & 0b1;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // Serial.println(state);
 
-  breakBeam = analogRead(A0);
-  tallBreakBeam = analogRead(A1);
-  // Serial.println(tallBreakBeam);
+  switch1Value = PINC & 0b1;
+
+  if ((switch1OldValue == 1) && (switch1Value == 0)) {
+    button = !button;
+    delay(button_delay);
+  } else if ((switch1OldValue == 0) && (switch1Value == 1)) {
+    delay(button_delay);
+  }
+
+  switch1OldValue = switch1Value;
+
+  breakBeamVal = analogRead(A0);
 
   duty = 255;
 
+  if (breakBeamVal >= beamTol) {
+    breakBeam = 1;
+  } else if (breakBeam < beamTol) {
+    breakBeam = 0;
+  }
+
   switch (state) {
-    case 1:  // forward
+
+    case 1:  // Rolling forward, arm at 0
+      desPos = 0;
       rollForward(duty);
-
-      if (breakBeam >= beamTol) {
+      if (button == 0)
+        state = 3;
+      else if (breakBeam == 1) {
         state = 2;
-        prevState = 1;
+        t_not = millis();
       }
 
       break;
 
-    case 2:  // stop
-      stopRolling();
-      desPos = 1600;
-
-
-
-      if (breakBeam < .9 * beamTol) {
-        if (prevState == 3) {
-          state = 1;
-
-        }
-
-        else if (prevState == 1) {
-          state = 3;
-        }
-        delay(debounceDT);
-        desPos = 0;
+    case 2:  // Rolling forward, arm at 45;
+      rollForward(duty);
+      desPos = 400;
+      if ((breakBeam == 0) && (millis() - t_not > 2000)) {
+        state = 1;
       }
       break;
 
-    case 3:  // reverse
+    case 3:  // Rolling backward, arm at 0
+      desPos = 0;
       rollBackward(duty);
-
-      if (breakBeam >= beamTol) {
-        state = 2;
-        prevState = 3;
+      if (button == 1) {
+        state = 1;
       }
       break;
   }
@@ -71,41 +87,40 @@ void loop() {
   if (curPos < desPos) {
     PORTA &= 0b0;
     PORTA &= 0b01;
-    delayMicroseconds(dt);
+    delayMicroseconds(stepper_dt);
     PORTA |= 0b10;
-    delayMicroseconds(dt);
+    delayMicroseconds(stepper_dt);
     curPos++;
 
   } else if (curPos > desPos) {
     PORTA |= 0b1;
     PORTA &= 0b01;
-    delayMicroseconds(dt);
+    delayMicroseconds(stepper_dt);
     PORTA |= 0b10;
-    delayMicroseconds(dt);
+    delayMicroseconds(stepper_dt);
     curPos--;
   }
-
 }
 
-  void pwm_init(void) {
-    DDRL = 255;
-    TCCR5A = _BV(COM5A1) | _BV(COM5B1) | _BV(WGM52) | _BV(WGM50);
-    TCCR5B = _BV(CS51) | _BV(CS50);
-    OCR5A = 0;
-    OCR5B = 0;
-  }
+void pwm_init(void) {
+  DDRL = 255;
+  TCCR5A = _BV(COM5A1) | _BV(COM5B1) | _BV(WGM52) | _BV(WGM50);
+  TCCR5B = _BV(CS51) | _BV(CS50);
+  OCR5A = 0;
+  OCR5B = 0;
+}
 
-  void rollForward(int duty) {
-    OCR5A = duty;
-    PORTB = 0b10;
-  }
+void rollForward(int duty) {
+  OCR5A = duty;
+  PORTB = 0b10;
+}
 
-  void rollBackward(int duty) {
-    OCR5A = duty;
-    PORTB = 0b1;
-  }
+void rollBackward(int duty) {
+  OCR5A = duty;
+  PORTB = 0b1;
+}
 
-  void stopRolling() {
-    OCR5A = 0;
-    PORTB = 0;
-  }
+void stopRolling() {
+  OCR5A = 0;
+  PORTB = 0;
+}
