598671563bcb87ccabb3241e7d97e2c19dbc7f38
jjv432 <jackvranicar43@gmail.com>
2025-01-26 13:38:58 -0500

    Simple SISO working with constant output

--- PLCSim/PLCSim.m
+++ PLCSim/PLCSim.m
@@ -2,7 +2,7 @@
 % plcladderlib is the command to access it
 
 % plcloadtypes to load in the built-in data types
-% plcladderoption('simpleController', 'Animation', 'on)
+% plcladderoption('<FILENAME.m>', 'Animation', 'on)
 
 % ctrl + d is used to update the model once the LL diagram is done
 
