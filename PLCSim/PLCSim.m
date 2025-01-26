f1cad0f99420f048e9fe58db6e7248ad6f110dbb
jjv432 <jackvranicar43@gmail.com>
2025-01-26 14:27:41 -0500

    State-based control done with two simple states, and SISO

--- PLCSim/PLCSim.m
+++ PLCSim/PLCSim.m
@@ -2,7 +2,7 @@
 % plcladderlib is the command to access it
 
 % plcloadtypes to load in the built-in data types
-% plcladderoption('<FILENAME.m>', 'Animation', 'on)
+% plcladderoption('<FILENAME>', 'Animation', 'on)
 
 % ctrl + d is used to update the model once the LL diagram is done
 
