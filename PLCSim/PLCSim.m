a8c1a9dd33416eb44eaa6e20a1b0a555649161a1
jjv432 <jackvranicar43@gmail.com>
2025-02-05 16:00:24 -0500

    Count up done

--- PLCSim/PLCSim.m
+++ PLCSim/PLCSim.m
@@ -5,4 +5,13 @@
 % plcladderoption('<FILENAME>', 'Animation', 'on)
 
 % ctrl + d is used to update the model once the LL diagram is done
+clc; clear; close all
+plcladderlib
+plcloadtypes
+Counters_Basic
+plcladderoption('Counters_Basic', 'Animation', 'on')
+
+% In order to do a counter, you need to go to the 'CTU_InitialValue' struct
+% in the workspace. Here, you can edit what PRE and ACC values are
+
 
