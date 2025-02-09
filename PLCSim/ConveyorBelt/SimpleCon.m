f7b3740e2cb7080f6b07fd85048f95c8c0531b50
jjv432 <jackvranicar43@gmail.com>
2025-02-09 16:20:57 -0500

    Redoing classes

---
+++ PLCSim/ConveyorBelt/SimpleCon.m
@@ -0,0 +1,8 @@
+% Plc Simscape
+% plcladderlib is the command to access it
+
+% plcloadtypes to load in the built-in data types
+% plcladderoption('simpleController', 'Animation', 'on)
+
+% ctrl + d is used to update the model once the LL diagram is done
+
