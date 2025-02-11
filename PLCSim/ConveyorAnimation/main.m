fb95ae83de26ac1aa140e2d70e92bad73acefd71
jjv432 <jackvranicar43@gmail.com>
2025-02-11 15:49:41 -0500

    Some form of the timer working

--- PLCSim/ConveyorAnimation/main.m
+++ PLCSim/ConveyorAnimation/main.m
@@ -18,35 +18,30 @@ end_time = 30;
 time_step = 0.2;
 
 times = start_time:time_step:end_time;
-simin = [boolean(ones(floor(numel(times)/2), 1)); boolean(zeros(ceil(numel(times)/2), 1))];
-
+% simin = [boolean(ones(floor(numel(times)/2), 1)); boolean(zeros(ceil(numel(times)/2), 1))];
+simin = ones(numel(times), 1);
+simin = boolean(simin);
 simin = timeseries(simin, times);
 
 %% Running simulation
-out = run_simulink_plc("MIMO", string(start_time), string(end_time), string(time_step));
+out = run_simulink_plc("SISO_Timer", string(start_time), string(end_time), string(time_step));
 % edit_simulink_plc is a good function to mess with the .slx
 
+% !! Need to make a way to take user input for time
 %% Simulation results
+% 
+% % Creating objects
+% c = conveyor_assembly(10, 1, 0, 0);
+% b = box();
+% c.draw_conveyor;
+% 
+% b.X_Position = 0;
+% b.Y_Position = 1;
+% 
+% % Running loop
+% for i = 1:numel(out.simout.Data)
+%     b.move_box(time_step, out.simout.Data(i), 0)
+% end
 
-% Creating objects
-c = conveyor_assembly(10, 1, 0, 0);
-b = box();
-c.draw_conveyor;
-
-b.X_Position = 0;
-b.Y_Position = 1;
-
-% Running loop
-for i = 1:numel(out.simout.Data)
-    b.move_box(time_step, out.simout.Data(i), 0)
-end
-
+%% Creating a figure to validate simout
 figure(); plot(out.simout); ylim([-.1 1.1])
-
-%{
-Next, can call the objects. Make a new input dt.  Make that an input to the
-run_simulink_plc as well.  Use the current output to control M1. Don't push
-box yet.  Maybe use of a counter would be a good trigger for when to push
-the box, i.e. move the box for 10s, then push it off (regardless of
-position)
-%}
