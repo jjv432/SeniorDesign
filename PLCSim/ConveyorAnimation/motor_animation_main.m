d72f7f6579f3c9009e29e6985023748b6a26ffee
jjv432 <jackvranicar43@gmail.com>
2025-02-11 22:16:00 -0500

    Animation adjusted

--- PLCSim/ConveyorAnimation/motor_animation_main.m
+++ PLCSim/ConveyorAnimation/motor_animation_main.m
@@ -14,22 +14,31 @@ axis([-5 5 -5 5]);
 
 omega = .1;
 direction = -1;
+dt = .1;
 
 button1 = timeseries([boolean(zeros(10, 1)); boolean(ones(20, 1))]); 
 button2 = timeseries([boolean(zeros(20, 1)); boolean(ones(10, 1))]);
 
-% out = run_simulink_plc("MIMO_FixedIO", '0', '30', '.1');
+out = run_simulink_plc("MIMO_FixedIO", '0', '30', string(dt));
 
 %% Running the simulation 
 
+vid = VideoWriter("MotorSimulation.avi");
+vid.FrameRate = 60;
+vid.Quality = 85;
+open(vid);
 
 for i = 1:numel(out.motor1.Data)
     omega1 = out.motor1.Data(i) * omega;
     omega2 = out.motor2.Data(i) * omega;
     m1.rotate_motor(omega1, direction);
     m2.rotate_motor(omega2, -direction);
+    writeVideo(vid, getframe(gcf));
+    delay(dt);
 end
 
+close(vid);
+%%
 figure(); 
 hold on; 
 plot(out.motor1.Data); 
