6c418ec1f894d961bf84930d22a8fc699d185c5d
jjv432 <jackvranicar43@gmail.com>
2025-03-18 16:41:40 -0400

    Expansion Module Folder (blank)

---
+++ OpenPLCFolders/ExpansionModule/build/POUS.c
@@ -0,0 +1,64 @@
+void LOGGER_init__(LOGGER *data__, BOOL retain) {
+  __INIT_VAR(data__->EN,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->ENO,__BOOL_LITERAL(TRUE),retain)
+  __INIT_VAR(data__->TRIG,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->MSG,__STRING_LITERAL(0,""),retain)
+  __INIT_VAR(data__->LEVEL,LOGLEVEL__INFO,retain)
+  __INIT_VAR(data__->TRIG0,__BOOL_LITERAL(FALSE),retain)
+}
+
+// Code part
+void LOGGER_body__(LOGGER *data__) {
+  // Control execution
+  if (!__GET_VAR(data__->EN)) {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(FALSE));
+    return;
+  }
+  else {
+    __SET_VAR(data__->,ENO,,__BOOL_LITERAL(TRUE));
+  }
+  // Initialise TEMP variables
+
+  if ((__GET_VAR(data__->TRIG,) && !(__GET_VAR(data__->TRIG0,)))) {
+    #define GetFbVar(var,...) __GET_VAR(data__->var,__VA_ARGS__)
+    #define SetFbVar(var,val,...) __SET_VAR(data__->,var,__VA_ARGS__,val)
+
+   LogMessage(GetFbVar(LEVEL),(char*)GetFbVar(MSG, .body),GetFbVar(MSG, .len));
+  
+    #undef GetFbVar
+    #undef SetFbVar
+;
+  };
+  __SET_VAR(data__->,TRIG0,,__GET_VAR(data__->TRIG,));
+
+  goto __end;
+
+__end:
+  return;
+} // LOGGER_body__() 
+
+
+
+
+
+void PROGRAM0_init__(PROGRAM0 *data__, BOOL retain) {
+  __INIT_LOCATED(BOOL,__QX0_8,data__->OUT,retain)
+  __INIT_LOCATED_VALUE(data__->OUT,__BOOL_LITERAL(FALSE))
+}
+
+// Code part
+void PROGRAM0_body__(PROGRAM0 *data__) {
+  // Initialise TEMP variables
+
+  __SET_LOCATED(data__->,OUT,,__BOOL_LITERAL(TRUE));
+
+  goto __end;
+
+__end:
+  return;
+} // PROGRAM0_body__() 
+
+
+
+
+
