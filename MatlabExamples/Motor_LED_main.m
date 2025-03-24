dc60372ae12847c4e5589865c4c8e9c377c2aec7
jjv432 <jackvranicar43@gmail.com>
2025-03-24 14:04:21 -0400

    Updating comments

--- MatlabExamples/Motor_LED_main.m
+++ MatlabExamples/Motor_LED_main.m
@@ -1,20 +1,36 @@
 clc; clear; close all;
+
+% This allows access to the PLC simulation functions
 addpath("src");
+
+%% Creating and plotting objects
+% Create two LED objects. These will be used to animate the system response
 l1 = LED(-1, 0, "Green");
 l2 = LED(1, 0, "Green");
 
+% Create a motor object. Useful for animating the system response.
+m = motor(1, 0, -1);
+
+% Create a new figure, then plot the initial positions of the LEDs and motors.
 figure();
 hold on
 axis equal
 l1.draw_LED;
 l2.draw_LED;
-m = motor(1, 0, -1);
 m.draw_motor()
 
 %% Creating inputs
+%{
+The behavior of these inputs is such that one button is on for 6 seconds, then off for 3, and the other is off for 3 seconds, then on for 
+6 seconds. The majority of these lines are used to put the inputs into the correct format for Simulink to accept them.
+%}
+
+% Make a 3 second chunk
 times = linspace(0, 3, 100);
+
+% 3, 3 second chunks
 tot_time = linspace(0, 9, 300);
-dt = times(2) - times(1);
+
 button_1_a = boolean(ones(numel(times), 1));
 button_2_a = boolean(zeros(numel(times), 1));
 
@@ -30,14 +46,20 @@ button_2 = [button_2_a; button_2_b; button_2_c];
 button_1 = timeseries(button_1, tot_time);
 button_2 = timeseries(button_2, tot_time);
 
+%% Simulate the system for 9 seconds
 out = run_simulink_plc("Motor_and_LED", '0', '9');
 
 %% Animating objects based on output
-vid = VideoWriter("ButtonAndLEDSimulation.avi");
-vid.FrameRate = 5;
-vid.Quality = 85;
-open(vid);
+
+% code to create a video file of this animation
+% vid = VideoWriter("ButtonAndLEDSimulation.avi");
+% vid.FrameRate = 5;
+% vid.Quality = 85;
+% open(vid);
+
 omega = 1;
+
+% For every element in the simulation output, check to see if the LEDs or the motor is on. If so, apply the appropriate animation method.
 for i = 1:numel(out.tout)
     if out.LED1.Data(i)
         l1.LED_On;
@@ -55,10 +77,10 @@ for i = 1:numel(out.tout)
     else
         pause(out.tout(2) - out.tout(1));
     end
-    writeVideo(vid, getframe(gcf));
+    % writeVideo(vid, getframe(gcf));
 end
 
-close(vid);
+%close(vid);
 
 %% Making figure of inputs
 
@@ -72,4 +94,4 @@ legend("Button 1", "Button 2")
 title("Simulated Button Inputs")
 ylabel("Logic Level");
 xlabel("Time (s)")
-saveas(gcf, "SimulateInputs.jpg");
+%saveas(gcf, "SimulateInputs.jpg");
