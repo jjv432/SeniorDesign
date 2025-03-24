762c9310880335944fd8d01237e45cf6bc9d5733
jjv432 <jackvranicar43@gmail.com>
2025-03-24 14:16:03 -0400

    Adding comments to motor_animation_main

--- MatlabExamples/motor_animation_main.m
+++ MatlabExamples/motor_animation_main.m
@@ -1,25 +1,32 @@
 clc; clearvars -except out; close all; format compact
 saveBool = 0;
 
+% Gives access to the simulation files
 addpath("src")
+
+% Create two motors and set their x-positions (y-positions default to 0)
 m1 = motor();
 m1.X_Position = 3;
 
 m2 = motor();
 m2.X_Position = -3;
 
+% Draw these two motors, and set the axis
 m1.draw_motor();
 m2.draw_motor();
 
 axis([-5 5 -5 5]);
 
+% Setting some info for how the motor will be animated
 omega = .1;
 direction = -1;
 dt = .1;
 
+% Forcing a timeseries of inputs for each button. This is doing button1 on for 10s off for 20s and button2 off for 20s on for 10.
 button1 = timeseries([boolean(zeros(10, 1)); boolean(ones(20, 1))]);
 button2 = timeseries([boolean(zeros(20, 1)); boolean(ones(10, 1))]);
 
+% Simulate the system using the simulink file MIMO_FixedIO.slx for 30 seconds
 out = run_simulink_plc("MIMO_FixedIO", '0', '30');
 
 %% Running the simulation
@@ -32,10 +39,15 @@ if saveBool
 end
 
 for i = 1:numel(out.motor1.Data)
+    % If the motor is supposed to be moving, set its speed to the omega we defined above
     omega1 = out.motor1.Data(i) * omega;
     omega2 = out.motor2.Data(i) * omega;
+
+    % Animate the motor if it's supposed to be moving
     m1.rotate_motor(omega1, direction);
     m2.rotate_motor(omega2, -direction);
+
+    % If you want to save the video 
     if saveBool
         writeVideo(vid, getframe(gcf));
     end
@@ -45,7 +57,8 @@ end
 if saveBool
     close(vid);
 end
-%%
+
+%% Plotting the simulation results
 figure();
 subplot(2, 1, 1)
 hold on
